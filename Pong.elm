12b170df726e2570b25c95a4ab28c9ef0d235c85
iver56 <iver56@hotmail.com>
2017-01-17 19:47:46 +0100

    Render initial game state

--- Pong.elm
+++ Pong.elm
@@ -1,11 +1,20 @@
 import Html exposing (..)
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+
 
 type alias Model =
-    {}
+    { ball : Ball
+    , paddleLeft : Paddle
+    , paddleRight : Paddle
+    }
 
 init : Model
 init =
-    {}
+    { ball = initBall
+    , paddleLeft = initPaddle 20
+    , paddleRight = initPaddle (boardWidth - 25)
+    }
 
 type Msg = Tick
 
@@ -15,9 +24,23 @@ update msg model =
         Tick ->
             model
 
+
 view : Model -> Html Msg
 view model =
-    text "Hello Pong!"
+    svg
+        [ width (toString boardWidth)
+        , height (toString boardHeight)
+        ]
+        [ rect
+            [ width (toString boardWidth)
+            , height (toString boardHeight)
+            , fill "black"
+            ]
+            []
+        , ballView model.ball
+        , paddleView model.paddleLeft
+        , paddleView model.paddleRight
+        ]
 
 main =
     Html.beginnerProgram
@@ -25,3 +48,67 @@ main =
         , update = update
         , view = view
         }
+
+boardWidth =
+    500
+
+boardHeight =
+    300
+
+type alias Ball =
+    { x : Float
+    , y : Float
+    , vx : Float
+    , vy : Float
+    , radius : Float
+    }
+
+type alias Paddle =
+    { x : Float
+    , y : Float
+    , vx : Float
+    , vy : Float
+    , width : Float
+    , height : Float
+    }
+
+initBall : Ball
+initBall =
+    { x = boardWidth / 2
+    , y = boardHeight / 2
+    , vx = 0.3
+    , vy = 0.3
+    , radius = 8
+    }
+
+initPaddle : Float -> Paddle
+initPaddle x =
+    { x = x
+    , y = 0
+    , vx = 0.4
+    , vy = 0.4
+    , width = 5
+    , height = 80
+    }
+
+
+ballView : Ball -> Svg Msg
+ballView model =
+    circle
+        [ cx (toString model.x)
+        , cy (toString model.y)
+        , r (toString model.radius)
+        , fill "white"
+        ]
+        []
+
+paddleView : Paddle -> Svg Msg
+paddleView model =
+    rect
+        [ width (toString model.width)
+        , height (toString model.height)
+        , x (toString model.x)
+        , y (toString model.y)
+        , fill "white"
+        ]
+        []
